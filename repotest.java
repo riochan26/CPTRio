cf2edfe4af05f027b57a6d82c57095e15d24fecd
riochan26 <152637961+riochan26@users.noreply.github.com>
2025-05-23 10:54:49 -0400

    added more stuf

--- repotest.java
+++ repotest.java
@@ -4,5 +4,7 @@ public class repotest{
 	public static void main(String[] args){
 		Console con = new Console();
 		con.println("Hello World");
+		con.println("tgif");
+		con.println("cpt time");
 	}
 }
